6b5c54a9ea3d676e847678e78ab74685126789ff
Easton Meth <easton.meth@gmail.com>
2024-05-16 19:34:30 -0400

    Episode 6 - Layouts

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -2,17 +2,12 @@
 
 namespace App\Filament\Resources;
 
-use App\Enums\Region;
 use App\Filament\Resources\ConferenceResource\Pages;
 use App\Models\Conference;
-use App\Models\Speaker;
-use App\Models\Venue;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 
 class ConferenceResource extends Resource
 {
@@ -23,49 +18,7 @@ class ConferenceResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->label('Conference')
-                    ->default('My Conference')
-                    ->helperText('The name of the conference.')
-                    ->required()
-                    ->maxLength(60),
-                Forms\Components\MarkdownEditor::make('description')
-                    ->helperText('Describe what this conference will be about.')
-                    ->required(),
-                Forms\Components\DateTimePicker::make('start_date')
-                    ->native(false)
-                    ->required(),
-                Forms\Components\DateTimePicker::make('end_date')
-                    ->native(false)
-                    ->required(),
-                Forms\Components\Toggle::make('is_published')
-                    ->default(true),
-                Forms\Components\Select::make('status')
-                    ->options([
-                        'draft' => 'Draft',
-                        'published' => 'Published',
-                        'archived' => 'Archived',
-                    ])
-                    ->required(),
-                Forms\Components\Select::make('region')
-                    ->live()
-                    ->enum(Region::class)
-                    ->options(Region::class),
-                Forms\Components\Select::make('venue_id')
-                    ->searchable()
-                    ->preload()
-                    ->createOptionForm(Venue::getForm())
-                    ->editOptionForm(Venue::getForm())
-                    ->relationship('venue', 'name', function (Builder $query, Forms\Get $get): Builder {
-                        return $query->where('region', $get('region'));
-                    }),
-                Forms\Components\CheckboxList::make('speakers')
-                    ->columnSpanFull()
-                    ->searchable()
-                    ->relationship('speakers', 'name')
-                    ->options(Speaker::all()->pluck('name', 'id')),
-            ]);
+            ->schema(Conference::getForm());
     }
 
     public static function table(Table $table): Table
--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,6 +2,19 @@
 
 namespace App\Models;
 
+use App\Enums\Region;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Forms\Get;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -32,5 +45,77 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Wizard::make()
+                ->columnSpanFull()
+                ->steps([
+                    Step::make('Conference Details')
+                        ->schema([
+                            TextInput::make('name')
+                                ->columnSpanFull()
+                                ->label('Conference')
+                                ->default('My Conference')
+                                ->required()
+                                ->maxLength(60),
+                            MarkdownEditor::make('description')
+                                ->columnSpanFull()
+                                ->required(),
+                            DateTimePicker::make('start_date')
+                                ->native(false)
+                                ->required(),
+                            DateTimePicker::make('end_date')
+                                ->native(false)
+                                ->required(),
+                            Fieldset::make('Status')
+                                ->columns(1)
+                                ->schema([
+                                    Select::make('status')
+                                        ->options([
+                                            'draft' => 'Draft',
+                                            'published' => 'Published',
+                                            'archived' => 'Archived',
+                                        ])
+                                        ->required(),
+                                    Toggle::make('is_published')
+                                        ->default(true),
+                                ]),
+                        ]),
+                    Step::make('Location')
+                        ->schema([
+                            Select::make('region')
+                                ->live()
+                                ->enum(Region::class)
+                                ->options(Region::class),
+                            Select::make('venue_id')
+                                ->searchable()
+                                ->preload()
+                                ->createOptionForm(Venue::getForm())
+                                ->editOptionForm(Venue::getForm())
+                                ->relationship('venue', 'name', function (Builder $query, Get $get): Builder {
+                                    return $query->where('region', $get('region'));
+                                }),
+                        ]),
+                    Step::make('Speakers')
+                        ->schema([
+                            CheckboxList::make('speakers')
+                                ->columnSpanFull()
+                                ->searchable()
+                                ->relationship('speakers', 'name')
+                                ->options(Speaker::all()->pluck('name', 'id')),
+                        ]),
+                ]),
+            // Section::make('Conference Details')
+            //     ->description('Provide some basic information about the conference.')
+            //     ->icon('heroicon-o-information-circle')
+            //     ->collapsible()
+            //     ->columns(2)
+
+            // Section::make('Location')
+            //     ->columns(2)
+        ];
     }
 }
