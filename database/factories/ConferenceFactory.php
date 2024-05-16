eac71d944755663235d1fa1ddbd50009a11994e3
Easton Meth <easton.meth@gmail.com>
2024-05-16 14:44:46 -0400

    Episode 1 - Create Models with Blueprint

---
+++ database/factories/ConferenceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Conference;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ConferenceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Conference::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'start_date' => $this->faker->word(),
+            'end_date' => $this->faker->word(),
+            'status' => $this->faker->word(),
+            'region' => $this->faker->word(),
+            'venue_id' => null,
+        ];
+    }
+}
