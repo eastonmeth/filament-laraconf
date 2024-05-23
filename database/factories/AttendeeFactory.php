27510cfbefefc6535e483bb7cbeaa5909d3959bd
Easton Meth <easton.meth@gmail.com>
2024-05-23 17:57:36 -0400

    Episode 15 - Widgets

--- database/factories/AttendeeFactory.php
+++ database/factories/AttendeeFactory.php
@@ -22,6 +22,8 @@ class AttendeeFactory extends Factory
             'email' => $this->faker->safeEmail(),
             'ticket_cost' => 50000,
             'is_paid' => true,
+            'conference_id' => 1,
+            'created_at' => $this->faker->dateTimeBetween('-3 months'),
         ];
     }
 
